4a38ebea6034bc4a325e185d8ebf8b513032230a
Luis Mata <69470188+lanmata@users.noreply.github.com>
2024-10-29 04:55:23 -0400

    JB-106 DataGrid Component to List the Companies
    [x] Added Company path in the app-const.ts file
    [x] Added styles in the company.component.css file
    [x] Implemented datagrid to show the company collection.
    [x] Updated unit test cases.
    [x] Updated company.component.ts file to load and display the company collection in the UI
    [x] Updated es.json file with the attributes for the company dashboard
    [x] Updated index.html file to include the content description

--- src/app/company/company.component.spec.ts
+++ src/app/company/company.component.spec.ts
@@ -1,61 +1,90 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {CompanyComponent} from './company.component';
-import { provideHttpClientTesting } from "@angular/common/http/testing";
+import {provideHttpClientTesting} from "@angular/common/http/testing";
 import {DebugElement} from "@angular/core";
 import {CompanyService} from "./company.service";
 import {of, throwError} from "rxjs";
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {HttpClient, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {TranslateFakeLoader, TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {DatePipe} from "@angular/common";
+import {ActivatedRoute, Router} from "@angular/router";
 
 describe('CompanyComponent', () => {
-  let component: CompanyComponent;
-  let fixture: ComponentFixture<CompanyComponent>;
-  let companyService: CompanyService;
-  let debugElement: DebugElement;
+    let component: CompanyComponent;
+    let fixture: ComponentFixture<CompanyComponent>;
+    let companyService: CompanyService;
+    let debugElement: DebugElement;
+    let mockActivatedRoute: ActivatedRoute;
+    let mockRouter: Router;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-    imports: [CompanyComponent],
-    providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
-})
-      .compileComponents();
+    beforeEach(async () => {
+        mockRouter = {
+            navigate: jasmine.createSpy('navigate'),
+            events: of({}) // Mock the events property
+        } as any;
+        mockActivatedRoute = {
+            snapshot: {
+                params: of({offerId: '0645d0de-fe0d-488c-920b-91145ac35387'})
+            }
+        } as any;
 
-    fixture = TestBed.createComponent(CompanyComponent);
-    debugElement = fixture.debugElement;
-    component = fixture.componentInstance;
-    companyService = debugElement.injector.get(CompanyService);
-    fixture.detectChanges();
-  });
+        await TestBed.configureTestingModule({
+            imports: [CompanyComponent,
+                BrowserAnimationsModule,
+                TranslateModule.forRoot({
+                    loader: {
+                        provide: TranslateLoader,
+                        useClass: TranslateFakeLoader
+                    }
+                })],
+            providers: [
+                {provide: ActivatedRoute, useValue: mockActivatedRoute},
+                {provide: HttpClient, useValue: jasmine.createSpyObj('httpClient', ['get', 'post'])},
+                {provide: DatePipe, useValue: jasmine.createSpyObj('DatePipe', ['transform'])},
+                provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting(),
+                {provide: Router, useValue: mockRouter}
+            ]
+        })
+            .compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(CompanyComponent);
+        debugElement = fixture.debugElement;
+        component = fixture.componentInstance;
+        companyService = debugElement.injector.get(CompanyService);
+        fixture.detectChanges();
+    });
 
-  it('should fetch companies on initialization', () => {
-    const companyServiceSpy = spyOn(companyService, 'getCompanies').and.callThrough();
-    component.ngOnInit();
-    expect(companyServiceSpy).toHaveBeenCalled();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-  it('should handle error while getting companies', () => {
-    const errorResponse = new Error('Error occurred while getting companies');
-    spyOn(companyService, 'getCompanies').and.returnValue(throwError(errorResponse));
-    component.ngOnInit();
-    expect(component.isErrorFound).toBeTrue();
-  });
+    it('should fetch companies on initialization', () => {
+        const companyServiceSpy = spyOn(companyService, 'getCompanies').and.callThrough();
+        component.ngOnInit();
+        expect(companyServiceSpy).toHaveBeenCalled();
+    });
 
-  it('should update data source when companies are fetched', () => {
-    const response = {companyTOCollection: [{id: 1, name: 'Company1'}, {id: 2, name: 'Company2'}]};
-    spyOn(companyService, 'getCompanies').and.returnValue(of(response));
-    component.ngOnInit();
-    expect(component.dataSource).toEqual(response.companyTOCollection);
-  });
+    it('should handle error while getting companies', () => {
+        const errorResponse = new Error('Error occurred while getting companies');
+        spyOn(companyService, 'getCompanies').and.returnValue(throwError(errorResponse));
+        component.ngOnInit();
+        expect(component.isErrorFound).toBeTrue();
+    });
 
-  it('should hide loader when companies are fetched', () => {
-    const response = {companyTOCollection: [{id: 1, name: 'Company1'}, {id: 2, name: 'Company2'}]};
-    spyOn(companyService, 'getCompanies').and.returnValue(of(response));
-    const loaderSpy = spyOn(component.loader, 'hide').and.callThrough();
-    component.ngOnInit();
-    expect(loaderSpy).toHaveBeenCalled();
-  });
+    it('should update data source when companies are fetched', () => {
+        const response = {companyTOCollection: [{id: 1, name: 'Company1'}, {id: 2, name: 'Company2'}]};
+        spyOn(companyService, 'getCompanies').and.returnValue(of(response));
+        component.ngOnInit();
+        expect(component.allData).toEqual(response.companyTOCollection);
+    });
+
+    it('should hide loader when companies are fetched', () => {
+        const response = {companyTOCollection: [{id: 1, name: 'Company1'}, {id: 2, name: 'Company2'}]};
+        spyOn(companyService, 'getCompanies').and.returnValue(of(response));
+        const loaderSpy = spyOn(component.loader, 'hide').and.callThrough();
+        component.ngOnInit();
+        expect(loaderSpy).toHaveBeenCalled();
+    });
 });
